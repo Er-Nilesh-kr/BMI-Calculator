33363f014eb4f0063015057955b1ccdc1711bce5
Nilesh <allmailtonilesh@gmail.com>
2021-03-19 22:56:08 +0530

    Create custom widgets

---
+++ lib/input_page.dart
@@ -0,0 +1,66 @@
+import 'package:bmi_calculator/icon_content.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:bmi_calculator/reusable_card.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Column(children: [
+        Expanded(
+            child: Row(
+          children: [
+            Expanded(
+              child: ReusableCard(
+                colour: activeCardColour,
+                cardChild:
+                    IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                colour: activeCardColour,
+                cardChild:
+                    IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE'),
+              ),
+            )
+          ],
+        )),
+        Expanded(
+          child: ReusableCard(colour: activeCardColour),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: ReusableCard(colour: activeCardColour),
+              ),
+              Expanded(
+                child: ReusableCard(colour: activeCardColour),
+              )
+            ],
+          ),
+        ),
+        Container(
+          color: bottomContainerColour,
+          margin: EdgeInsets.only(top: 10.0),
+          width: double.infinity,
+          height: bottomContainerHeight,
+        )
+      ]),
+    );
+  }
+}
