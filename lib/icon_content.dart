434cd94fd0db3267efe838907ef332c65f7c993b
Nilesh <allmailtonilesh@gmail.com>
2021-03-21 19:15:16 +0530

    Move constants to constant.dart

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,9 +1,5 @@
+import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
-
-const labelTextStyle = TextStyle(
-  fontSize: 18.0,
-  color: Colors.white54,
-);
 
 class IconContent extends StatelessWidget {
   final IconData icon;
@@ -27,7 +23,7 @@ class IconContent extends StatelessWidget {
         ),
         Text(
           label,
-          style: labelTextStyle,
+          style: klabelTextStyle,
         )
       ],
     );
