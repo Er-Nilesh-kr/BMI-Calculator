4699e02866f8108d5b2883950ec4a39402fdf480
Nilesh <allmailtonilesh@gmail.com>
2021-03-21 19:15:43 +0530

    Add height card

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,16 +2,12 @@ import 'package:bmi_calculator/icon_content.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bmi_calculator/reusable_card.dart';
+import 'package:bmi_calculator/constants.dart';
 
 enum Gender {
   male,
   female,
 }
-
-const bottomContainerHeight = 80.0;
-const activeCardColour = Color(0xFF1D1E33);
-const inactiveCardColour = Color(0xFF111328);
-const bottomContainerColour = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -28,7 +24,7 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: Column(children: [
+      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
         Expanded(
             child: Row(
           children: [
@@ -40,8 +36,8 @@ class _InputPageState extends State<InputPage> {
                   });
                 },
                 colour: selectedGender == Gender.male
-                    ? activeCardColour
-                    : inactiveCardColour,
+                    ? kactiveCardColour
+                    : kinactiveCardColour,
                 cardChild: IconContent(
                   icon: FontAwesomeIcons.mars,
                   label: 'MALE',
@@ -59,8 +55,8 @@ class _InputPageState extends State<InputPage> {
                   });
                 },
                 colour: selectedGender == Gender.female
-                    ? activeCardColour
-                    : inactiveCardColour,
+                    ? kactiveCardColour
+                    : kinactiveCardColour,
                 cardChild: IconContent(
                   icon: FontAwesomeIcons.venus,
                   label: 'FEMALE',
@@ -73,25 +69,43 @@ class _InputPageState extends State<InputPage> {
           ],
         )),
         Expanded(
-          child: ReusableCard(colour: activeCardColour),
+          child: ReusableCard(
+            colour: kactiveCardColour,
+            cardChild: Column(
+              children: [
+                Text(
+                  'HEIGHT',
+                  style: klabelTextStyle,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '180',
+                      style: kNumberTextSyle,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
         Expanded(
           child: Row(
             children: [
               Expanded(
-                child: ReusableCard(colour: activeCardColour),
+                child: ReusableCard(colour: kactiveCardColour),
               ),
               Expanded(
-                child: ReusableCard(colour: activeCardColour),
+                child: ReusableCard(colour: kactiveCardColour),
               )
             ],
           ),
         ),
         Container(
-          color: bottomContainerColour,
+          color: kbottomContainerColour,
           margin: EdgeInsets.only(top: 10.0),
           width: double.infinity,
-          height: bottomContainerHeight,
+          height: kbottomContainerHeight,
         )
       ]),
     );
