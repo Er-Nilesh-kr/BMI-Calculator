951a042418f6c1868e46bb954820243c0b145442
Nilesh <allmailtonilesh@gmail.com>
2021-03-22 23:59:40 +0530

    Enhance colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const kbottomContainerHeight = 80.0;
 const kactiveCardColour = Color(0xFF1D1E33);
 const kinactiveCardColour = Color(0xFF111328);
-const kbottomContainerColour = Color(0xFFEB1555);
+const kbottomContainerColour = Color(0xFF330066);
 
 const klabelTextStyle = TextStyle(
   fontSize: 18.0,
