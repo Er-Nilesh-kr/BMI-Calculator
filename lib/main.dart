c8eb57506f3d62e2fcf9a6435c186436ca6aa74d
Nilesh <allmailtonilesh@gmail.com>
2021-03-23 00:46:15 +0530

    Enhance color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        primaryColor: Color(0xFF11131a),
+        scaffoldBackgroundColor: Color(0xFF11131a),
       ),
       home: InputPage(),
     );
