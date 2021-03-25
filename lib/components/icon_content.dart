e2f07869d5e6df7f4df5bda1da641776e10f726a
Nilesh <allmailtonilesh@gmail.com>
2021-03-25 07:31:55 +0530

    Modify icons

---
+++ lib/components/icon_content.dart
@@ -0,0 +1,31 @@
+import 'package:bmi_calculator/components/constants.dart';
+import 'package:flutter/material.dart';
+
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final Color icolor;
+
+  IconContent({this.icon, this.label, this.icolor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 76.0,
+          color: icolor,
+        ),
+        SizedBox(
+          height: 13.0,
+        ),
+        Text(
+          label,
+          style: klabelTextStyle,
+        )
+      ],
+    );
+  }
+}
