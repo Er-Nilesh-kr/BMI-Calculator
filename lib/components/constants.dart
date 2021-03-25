398d4aaddda6cba3f75fcafa9ab4f418ed2dabed
Nilesh <allmailtonilesh@gmail.com>
2021-03-25 23:57:53 +0530

    Modify widget style

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 const kbottomContainerHeight = 80.0;
 const kactiveCardColour = Color(0xFF1D1E33);
 const kinactiveCardColour = Color(0xFF111328);
-const kbottomContainerColour = Color(0xFF330066);
 
 const klabelTextStyle = TextStyle(
   fontSize: 18.0,
@@ -23,13 +22,13 @@ const kLargeButtonTextStyle = TextStyle(
 );
 
 const kTitleTextStyle = TextStyle(
-  fontSize: 50.0,
+  fontSize: 35.0,
   fontWeight: FontWeight.bold,
 );
 
 const kResultTextStyle = TextStyle(
   color: Color(0xFF24D876),
-  fontSize: 22.0,
+  fontSize: 26.0,
   fontWeight: FontWeight.bold,
 );
 
